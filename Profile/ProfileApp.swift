bbb823a5112d24d2bd689a63827800e8aaefb0bb
Desiree Jallow <60536540+desiree-jallow@users.noreply.github.com>
2021-04-20 14:26:18 -0500

    Initial Commit

---
+++ Profile/ProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileApp.swift
+//  Profile
+//
+//  Created by Desiree on 4/20/21.
+//
+
+import SwiftUI
+
+@main
+struct ProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
